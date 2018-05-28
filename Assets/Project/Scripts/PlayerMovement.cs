d0e55cf26a53deb8a867d580a61098977d73481f
hitori <innpol@rambler.ru>
2018-05-28 19:03:49 +0300

    Added 2.5D scene and main player model

--- Assets/Project/Scripts/PlayerMovement.cs
+++ Assets/Project/Scripts/PlayerMovement.cs
@@ -10,83 +10,101 @@ public class PlayerMovement : MonoBehaviour
 	float movementRunSpeed = 15f;
 	float movementAmount;
 	bool isPlayerFacingRight = true;
+	bool isHeadFacingAsBody = true;
 	float headRotationSpeed = 10f;
+	float fieldOfView = 130f;
 	Vector2 velocity;
 	Rigidbody2D rb;
 
 	//for sight
-	public GameObject head;
+	public Transform body;
+	public Transform head;
 
 
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody2D> ();
+		float movementSpeed;
 	}
 	
 
 	void Update () 
 	{
+		PlayerSight ();
+
 		movementAmount = Input.GetAxis ("Horizontal") * movementWalkSpeed;
 		if (Input.GetKey (KeyCode.LeftShift)) {
 			movementAmount = Input.GetAxis ("Horizontal") * movementRunSpeed;
 		}
 
-		PlayerSight ();
+		if (movementAmount < 0f && isPlayerFacingRight) {
+			FlipPlayerBody ();
+		} else if (movementAmount > 0f && !isPlayerFacingRight)
+			FlipPlayerBody ();
 	}
 
 	private void FixedUpdate()
 	{
 		velocity = rb.velocity;
 		velocity.x = movementAmount;
-		rb.velocity = velocity;
-
-		if (movementAmount < 0f && isPlayerFacingRight) {
-			FlipPlayer ();
-		} else if (movementAmount > 0f && !isPlayerFacingRight)
-			FlipPlayer ();
+		rb.velocity = velocity; // *time.fixeddeltatime???
 	}
 
-	void FlipPlayer()
+
+	void FlipPlayerBody()
 	{
 		isPlayerFacingRight = !isPlayerFacingRight;
-		Vector2 flippedLocalScale = this.transform.localScale;
-		flippedLocalScale.x *= -1;// THINK ABOUT FLIPPING PLAYER WITH SOME SPEED, NOT JUST INSTATNT
-		this.transform.localScale = flippedLocalScale;
+		body.RotateAround (body.position, Vector3.up, 180);
+
+
+		// 		FLIP THROUGHT SCALE CHANGING
+		//
+		//		Vector2 flippedLocalScale = this.transform.localScale;
+		//		flippedLocalScale.x *= -1;// THINK ABOUT FLIPPING PLAYER WITH SOME SPEED, NOT JUST INSTATNT
+		//		this.transform.localScale = flippedLocalScale;
 	}
+
+
+	void FlipPlayerHead()
+	{
+
+	}
+
 
 	void PlayerSight()
 	{
-		//main sight controls
-//		Vector2 mousePosition = Input.mousePosition;
-//		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
-//		Vector3 direction = new Vector3 (mousePosition.x - head.transform.position.x , mousePosition.y - head.transform.position.y, 0);
-//
-//
-//		if (isPlayerFacingRight)
-//			head.transform.right = direction;
-//		else
-//			head.transform.right = -direction;
+		float angleHB = Vector3.Angle (head.right, body.right);
+		if (angleHB < 90)
+			isHeadFacingAsBody = true;
+		else if (angleHB >= 90)
+			isHeadFacingAsBody = false;
+		Debug.Log (angleHB);
 
-
-		Vector3 mousePosition = Input.mousePosition;
+		Vector2 mousePosition = Input.mousePosition;
 		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
-		mousePosition.z = 0;
-		Vector3 direction = mousePosition - head.transform.position;
-		Quaternion lookRotation = Quaternion.LookRotation (direction);
-		Vector3 rotation = Quaternion.Lerp (head.transform.rotation, lookRotation, Time.deltaTime * headRotationSpeed).eulerAngles;
-		rotation.z = Mathf.Clamp (rotation.z, 0, 90);
-		head.transform.rotation = Quaternion.Euler (0f, 0f, rotation.z);
+		Vector2 direction = mousePosition - new Vector2(head.position.x, head.position.y);
+		direction.Normalize ();
+		float angle = Vector3.Angle (direction, body.right);
+
+		float rotZ = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		head.rotation = Quaternion.Euler (0, 0, rotZ);
+		if (!isHeadFacingAsBody) {
+			head.RotateAround (head.position, Vector3.up, 180);
+			isHeadFacingAsBody = true;
+		}
+			
+		
 
 
 
-
-
-
-
-		//Sight restrictions
-//		float angle = Vector3.Angle(direction, transform.right);
-//		Mathf.Clamp (angle, 0, 90);
-//		Debug.Log (angle);
-
+//		if (isHeadFacingAsBody && angle < fieldOfView * 0.5f) {
+//			float rotZ = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+//			head.rotation = Quaternion.Euler (0, 0, rotZ);
+//		} 
+//
+//		else if (!isHeadFacingAsBody && angle > 130 && angle < 180) 
+//		{
+//			head.RotateAround (head.position, Vector3.up, 180);
+//		}
 	}
 }
